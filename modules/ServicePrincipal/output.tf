6b8f0b97a96febdbff84386c2a967c6343833d03
khan962022 <112611477+khan962022@users.noreply.github.com>
2024-01-03 12:19:13 +0530

    Update output.tf
    sensitive = true

--- modules/ServicePrincipal/output.tf
+++ modules/ServicePrincipal/output.tf
@@ -6,24 +6,28 @@ output "service_principal_name" {
 output "service_principal_object_id" {
   description = "The object id of service principal. Can be used to assign roles to user."
   value       = azuread_service_principal.main.object_id
+  sensitive = true
 }
 
 output "service_principal_tenant_id" {
   value = azuread_service_principal.main.application_tenant_id
+  sensitive = true
 }
 
 output "service_principal_application_id" {
   description = "The object id of service principal. Can be used to assign roles to user."
   value       = azuread_service_principal.main.application_id
+  sensitive = true
 }
 
 output "client_id" {
   description = "The application id of AzureAD application created."
   value       = azuread_application.main.application_id
+  sensitive = true
 }
 
 output "client_secret" {
   description = "Password for service principal."
   value       = azuread_service_principal_password.main.value
- 
+  sensitive = true
 }
